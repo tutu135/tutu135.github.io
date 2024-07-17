0b1d06254d6f978da9c3b1609fb224ae32e8b245
Jimmy <135952752+tutu135@users.noreply.github.com>
2024-07-17 20:18:42 +0800

    Update settings.ts

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
-  site: 'https://simple-blog.littlesticks.dev',
-  name: 'Simple Blog',
-  title: 'Site title',
-  description: 'Site description',
+  site: 'https://tutu135.github.io',
+  name: 'Jimmy Blog',
+  title: 'Hello Everyone!',
+  description: 'Welcome to my blog!',
   showLittleSticksPlug: true,
 }
